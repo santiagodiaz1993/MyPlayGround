4b0a2cd5ca48ebd6e539ab4bb8b819f5af3318c7
Santiago <sdiaz19@binghamton.edu>
2019-12-01 22:45:48 -0500

    Adding the final touches for an application that will allow me to iput information.

--- WebApp/app.py
+++ WebApp/app.py
@@ -7,34 +7,59 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-	id = db.Column(db.Integer, primary_key =True)
-	content = db.Column(db.String(200), nullable = False)
-	completed = db.Column(db.Integer, default=0)
-	date_created = db.Column(db.DateTime, default =datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-	def __repr__(self):
-		return '<Task %r>' % self.id
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-	# This is going to grab the input from the from from index.html 
-	if request.method == "POST":
-		task_content = request.form['content']
-		new_task = Todo(content=task_content)
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
-		# add the variable new_task to the database 
-		try:
-			db.session.add(new_task)
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'there was an issue adding your task!'
-	
-	else:
-		tasks = Todo.query.order_by(Todo.date_created).all()
-		return render_template('index.html', tasks = tasks)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
-if __name__== "__main__":
-	app.run(debug=True)
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
