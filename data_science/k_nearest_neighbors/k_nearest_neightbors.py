51d9b307e7a09296e8cddb44ad3c42efbd9c152d
santiagodiaz1993 <santiagobmx@hotmail.es>
2020-01-02 19:31:19 -0500

    Adding a file with a summary of first non practial video for k nearest neightboors.

---
+++ data_science/k_nearest_neighbors/k_nearest_neightbors.py
@@ -0,0 +1,29 @@
+"""
+RECAP:
+    With linear regression the objective was to create a model that best fits our data. 
+
+    
+OBJECTIVE:
+    The objextive with classification is to create a model that best devides or seperates our data.
+
+NOTES:
+The objective of this model is to devide or separate our data. It separates points into seperate groups. It closters!. 
+
+Classification != clustering. Clustering is assinging points to seperate groups.In classification the objective is to create a model that properly devides groups, but also predicts which group does a pint belong. A algorithm can be chosen for this.
+
+K = the number of closest points. 
+
+Then a percentage is calculated from the types of K nearest points. 
+
+Confidence equals the percentage of same types points found within the closest  K points. 
+
+Confidence does not equal accuracy. 
+
+Euclid discanec is what used to estimate the closest K points. 
+
+k nearest nightboors is not very good for scaling. Estimations can also be done in paralle
+
+most macine learning algorithms want a category to be a number that represents a type.
+"""
+
+
