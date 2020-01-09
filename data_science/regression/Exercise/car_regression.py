07c5a643040fd5bd3445a25a521f42385b089c23
Santiago <sdiaz19@binghamton.edu>
2020-01-08 19:08:26 -0500

    Working on my first linear regression problem. I was able to select the information and apply the regression model but the accuracy is very low

--- data_science/regression/Exercise/car_regression.py
+++ data_science/regression/Exercise/car_regression.py
@@ -1,6 +1,6 @@
 '''
 Questions we want to answer:
-   What is the  in the future?
+   What is the price in the future?
 Steps we need to take:
 Lable the information [done]
 Inspect information and get most important attributes
@@ -10,22 +10,32 @@ Drop none significant information
 
 import pandas as pd 
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('imports-85.data')
-print(df.shape)
-print(df)
+df.dropna(inplace=True)
 
-X = df['price'].replace('?', 0)
-X = pd.to_numeric(X)
-y = df['symboling']
+X = df[['compression_ratio', 'city_mpg', 'peak_rpm']].sort_values('compression_ratio', ascending=True)
+X = X.replace('?', 100)
+X = np.array(X) 
 
-print('This is the price matrix')
-print(X)
-print('This is the symboling')
-print(y)
+y = df['price'].replace('?', 100)
+y = pd.to_numeric(y)
+y = np.array(y)
+# X = preprocessing.scale(X)
 
-plt.scatter(X, y)
-plt.title('Price vs Symboling')
-plt.xlabel('Price')
-plt.ylabel('symboling')
-plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+# plt.scatter(y, X)
+plt.title('Price vs highway mpg')
+plt.xlabel('Symboling')
+plt.ylabel('Price')
+# plt.show()
