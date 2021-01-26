fd7d62ed1750b6d5e999263b26b782c94cb2a997
Santiago Diaz <sdiaz19@binghamton.edu>
2021-01-26 15:35:23 -0500

    continuation of the programing of the backpropagation

--- data_science/deep_learning/neural_networks_from_scratch/backpropagation_example.py
+++ data_science/deep_learning/neural_networks_from_scratch/backpropagation_example.py
@@ -1,6 +1,6 @@
 # back propagation on a single neuron without the use of any library
 
-# foward pass
+# ----foward pass----
 x = [1, -2, 3]
 w = [-3, -1, 2]
 b = 1
@@ -16,7 +16,7 @@ z = xw0 + xw1 + xw2 + b
 # Relu activation function
 y = max(z, 0)
 
-
+# -----backpropagation------
 # back propagations starts here assuming the derivative from the next layers is
 # 1
 dvalue = 1
@@ -25,7 +25,7 @@ dvalue = 1
 # derivative value form the next layer times current layer after doing the
 # derivative and pluging in the z value
 drelu_dz = dvalue * (1 if z > 0 else 0)
-
+print(drelu_dz)
 # Partial derivatives of the multiplication, the chain rule
 # The derivative of all these values are the partial derivatives of
 # z = xw0 + xw1 + xw2 + b
@@ -33,9 +33,25 @@ dsum_dxw0 = 1
 dsum_dxw1 = 1
 dsum_dxw2 = 1
 dsum_db = 1
+
 # All of the them follow a similar partial bc are in the same function
 drelu_dxw0 = drelu_dz * dsum_dxw0
 drelu_dxw1 = drelu_dz * dsum_dxw1
 drelu_dxw2 = drelu_dz * dsum_dxw2
 drelu_db = drelu_dz * dsum_db
 print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+# Partial derivatives of the multiplication, the chain rule
+dmul_dx0 = w[0]  # -3
+dmul_dx1 = w[1]  # -1
+dmul_dx2 = w[2]  # 2
+dmul_dw0 = x[0]  # 1
+dmul_dw1 = x[1]  # -2
+dmul_dw2 = x[2]  # 3
+drelu_dx0 = drelu_dxw0 * dmul_dx0
+drelu_dw0 = drelu_dxw0 * dmul_dw0
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
+drelu_dw2 = drelu_dxw2 * dmul_dw2
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
