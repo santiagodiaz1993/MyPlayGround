b6ae3bfb8f9a54616321df0e68693b3974080139
Santiago Diaz <sdiaz19@binghamton.edu>
2021-04-21 01:33:35 -0400

    added missing part from previous commit due to not saving

--- data_science/deep_learning/neural_networks_from_scratch/NNFS1.py
+++ data_science/deep_learning/neural_networks_from_scratch/NNFS1.py
@@ -583,6 +583,28 @@ class Activation_Lienar:
         self.dinputs = dvalues.copy()
 
 
+class Loss_MeanSquare(Loss):
+
+    # forward pass
+    def forward(self, y_pred, y_true):
+        # Calculate loss
+        sample_losses = np.mean((y_true - y_pred) ** 2, axis=1)
+        # Return losses
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+        # Number of ouputs in every sample
+        # Well use the first sample to count them
+        ouputs = len(dvalues[0])
+
+        # Gradients on values
+        self.inputs = -2 * (y_true - davlues) / ouputs
+        # Normalize gradient
+        self.inputs = self.inputs / samples
+
+
 #  Create data set
 X, y = spiral_data(samples=100, classes=2)
 
